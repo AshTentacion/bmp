6e73185ecb0448177b982dc53f56d0467b2a8cde
AshenTen <791186615@qq.com>
2020-11-26 10:28:44 +0800

    update

--- web.py
+++ web.py
@@ -4,12 +4,11 @@ import json
 import time
 
 app = Flask(__name__)
-test_db = Db()
-test_db.create_table()
 
 
 @app.route('/')
 def get_all_infos():
+    print("heell")
     ret = test_db.select_all()
     dict = {}
     for index, data in enumerate(ret):
@@ -19,7 +18,7 @@ def get_all_infos():
         data_dict['pressure'] = data[2]
         data_dict['time'] = data[3].strftime("%Y-%m-%d %H:%M:%S")
         data_dict['creator'] = data[4]
-        dict['data'+str(index)] = data_dict
+        dict['data' + str(index)] = data_dict
     return json.dumps(dict)
 
 
@@ -32,3 +31,13 @@ def insert_data():
     test_db.insert(data)
 
     return 'ok'
+
+
+if __name__ == '__main__':
+    test_db = Db()
+    test_db.create_table()
+    app.run(
+        host='0.0.0.0',
+        port=80,
+        debug=True
+    )
